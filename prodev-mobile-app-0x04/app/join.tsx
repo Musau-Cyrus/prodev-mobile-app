86389adcad97747b326c60847c2ee83cd8e91eb1
Musau-Cyrus <eb3.61528.22@student.chuka.ac.ke>
2025-07-16 23:27:44 +0300

    Updated the create account page

--- prodev-mobile-app-0x04/app/join.tsx
+++ prodev-mobile-app-0x04/app/join.tsx
@@ -11,10 +11,10 @@ export default function Index() {
           <Ionicons name="arrow-back" size={25} />
           <Image source={require('@/assets/images/logo.png')} />
         </View>
-        <Text style={styles.titleText}>Sign in to your</Text>
+        <Text style={styles.titleText}>Create Your</Text>
         <Text style={styles.titleText}>Account</Text>
         <Text style={styles.subText}>
-          Enter your email and password to sign in.
+          Enter your details to create a new account.
         </Text>
 
         <View style={styles.formGroup}>
@@ -27,13 +27,19 @@ export default function Index() {
             <View style={styles.formPasswordControl}>
               <TextInput style={{ flex: 1 }} />
               <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
-            </View>
+            </View>  
           </View>
-          <Text style={styles.forgotPasswordText}>Forgot password?</Text>
+          <View style={{ marginTop: 20 }}>
+            <Text style={styles.formLabel}>Password</Text>
+            <View style={styles.formPasswordControl}>
+              <TextInput style={{ flex: 1 }} />
+              <FontAwesome name="eye-slash" size={24} color="#7E7B7B" />
+            </View>  
+          </View>
         </View>
 
         <TouchableOpacity style={styles.primaryButton}>
-          <Text style={styles.buttonText}>Sign in</Text>
+          <Text style={styles.buttonText}>Create</Text>
         </TouchableOpacity>
 
         <View style={styles.dividerGroup}>
